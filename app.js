3981bcf1d69e3b51df8f7d8be878004c6114fd7a
Tushar Joshi <tushar_12013113@nitkkr.ac.in>
2022-08-27 16:09:01 +0530

    ready to try deploying

--- app.js
+++ app.js
@@ -96,6 +96,7 @@ app.use((err,req,res,next) => {
     res.status(statusCode).render('error', {err});
 })
 
-app.listen(3000, () => {
-    console.log('Port');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(port);
 })
